0d1256d6c27760c53a48a650856ca6dbd0aef576
Dayane Santos <dayane0204@hotmail.com>
2023-08-15 12:04:01 -0300

    Criando o estado de troca dos botões

---
+++ src/components/Tarefas/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import * as S from './styles'
+
+type Props = {
+  descricao: string
+  status: string
+  titulo: string
+  prioridade: string
+}
+
+const Tarefa = ({ descricao, titulo, prioridade, status }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag>{prioridade}</S.Tag>
+      <S.Tag>{status}</S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao onClick={() => setEstaEditando(false)}>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+
+export default Tarefa
--- src/containers/Lista de Tarefas/index.tsx
+++ src/containers/Lista de Tarefas/index.tsx
@@ -1,34 +1,47 @@
-import Tarefa from '../../components/FiltroCard/Tarefas'
+import Tarefa from '../../components/Tarefas'
 import { Container } from './styles'
+
+const listaTarefas = [
+  {
+    titulo: 'Título 1',
+    descricao: 'Descrição 1',
+    prioridade: 'Importante',
+    status: 'Concluida'
+  },
+  {
+    titulo: 'Título 2',
+    descricao: 'Descrição 2',
+    prioridade: 'Importante',
+    status: 'Pendente'
+  },
+  {
+    titulo: 'Título 3',
+    descricao: 'Descrição 3',
+    prioridade: 'Normal',
+    status: 'Concluida'
+  },
+  {
+    titulo: 'Título 3',
+    descricao: 'Descrição 3',
+    prioridade: 'Urgente',
+    status: 'Pendente'
+  }
+]
 
 const ListaTarefas = () => (
   <Container>
     <p>X tarefas marcadas como: --categoria-- e --termo--</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {listaTarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            titulo={t.titulo}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
