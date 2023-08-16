b4db293554ddde4dc0dc10bd9aaf0643a6aaad37
Dayane Santos <dayane0204@hotmail.com>
2023-08-16 17:09:29 -0300

    Configurando o React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Provider } from 'react-redux'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ListaTarefas from './containers/Lista de Tarefas'
 import Aside from './containers/aside'
 import EstiloGlobal, { Container } from './styles'
 import store from './store'
+import Home from './pages/home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/nova',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <Aside />
-        <ListaTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import ListaTarefas from '../../containers/Lista de Tarefas'
+import Aside from '../../containers/aside'
+
+const Home = () => (
+  <>
+    <Aside />
+    <ListaTarefas />
+  </>
+)
+
+export default Home
